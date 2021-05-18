33b09eeccfecc2c79f905e1339c1bdeb9fe54566
Hari Narayanan Muthuswamy <harinarayanan.m@thirdware.com>
2021-05-18 13:56:13 +0530

    second commit

--- src/com/thirdware/smaplepro/NewSampleClass.java
+++ src/com/thirdware/smaplepro/NewSampleClass.java
@@ -3,5 +3,6 @@ package com.thirdware.smaplepro;
 public class NewSampleClass {
 	public static void main(String[] args) {
 		System.out.println("New Sample Class");
+		System.out.println("New Line");
 	}
 }
---
+++ src/com/thirdware/smaplepro/SecondClass.java
@@ -0,0 +1,7 @@
+package com.thirdware.smaplepro;
+
+public class SecondClass {
+public static void main(String[] args) {
+	System.out.println("Second Class");
+}
+}
